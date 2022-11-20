20735b146390c2d61db399d23d8ca9d31acd3932
pasko3101 <pasko3101@gmail.com>
2022-11-20 22:42:36 +0200

    footer -> brends

---
+++ src/component/footer/Brends.js
@@ -0,0 +1,18 @@
+import newBalance from "./picture/new-balance.svg";
+import drMartens from "./picture/dr-martens.svg";
+import converse from "./picture/converse.svg";
+import steveMadden from "./picture/steve-madden.svg";
+import uggAustralia from "./picture/ugg-australia.svg";
+function Brends() {
+  return (
+    <div className="footer__brends">
+      <img src={newBalance} alt="newBalance" />
+      <img src={drMartens} alt="drMartens" />
+      <img src={converse} alt="converse" />
+      <img src={steveMadden} alt="steveMadden" />
+      <img src={uggAustralia} alt="uggAustralia" />
+      <div className="text__os_regular bl">All brands</div>
+    </div>
+  );
+}
+export default Brends;
--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,4 +1,11 @@
+import image237 from "./picture/image237.png";
+import Brends from "./Brends";
 function Ftr() {
-  return <></>;
+  return (
+    <div className="footer">
+      <img src={image237} alt="image237" />
+      <Brends></Brends>
+    </div>
+  );
 }
 export default Ftr;
