493cda4d93caaed7ab8594eab0fc99cea71368e6
pasko3101 <pasko3101@gmail.com>
2022-11-20 22:10:37 +0200

    main fuul components

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -3,17 +3,19 @@ import NewArrivals from "./NewArrivals";
 import image234 from "./picture/image234.png";
 import image235 from "./picture/image235.png";
 import OurPicks from "./OurPicks";
+import EcoCollection from "./EcoCollection";
 function Mn() {
   return (
     <div className="main">
-        <Heroscreen></Heroscreen>
+      <Heroscreen></Heroscreen>
       <div className="main__container">
         <NewArrivals></NewArrivals>
         <div className="main__pictures">
           <img className="main__pictures_img" src={image234} alt="image234" />
-          <img className="main__pictures_img" src={image235} alt="image234" />
+          <img className="main__pictures_img" src={image235} alt="image235" />
         </div>
-				<OurPicks></OurPicks>
+        <OurPicks></OurPicks>
+        <EcoCollection></EcoCollection>
       </div>
     </div>
   );
